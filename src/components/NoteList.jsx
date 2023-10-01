905e5ba7667683954ef3d95756757f2eeae81f44
myuzara12 <myuzara12@gmail.com>
2023-10-01 08:42:55 +0700

    fix text to center

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -3,21 +3,25 @@ import NoteItem from './NoteItem'
 
 const NoteList = ({ noteDatas, onDelete, onArchive }) => {
     return (
-        <div className="notes-list">
+        <>
             {noteDatas.length === 0 ? (
                 <p className="notes-list__empty-message">Note is empty</p>
             ) : (
-                noteDatas.map((noteData) => (
-                    <NoteItem
-                        key={noteData.id}
-                        id={noteData.id}
-                        onDelete={onDelete}
-                        onArchive={onArchive}
-                        {...noteData}
-                    />
-                ))
+                <div className='notes-list'>
+                    {
+                        noteDatas.map((noteData) => (
+                            <NoteItem
+                                key={noteData.id}
+                                id={noteData.id}
+                                onDelete={onDelete}
+                                onArchive={onArchive}
+                                {...noteData}
+                            />
+                        ))
+                    }
+                </div>
             )}
-        </div>
+        </>
     )
 }
 
